281de2aaf4bc79e44b3ddde2a0b23b14c86bea7a
silviojpa <silviop.ezequiel@gmail.com>
2023-01-05 22:48:11 -0300

    first commit

---
+++ Creat Auto de VMs.ps1
@@ -0,0 +1,16 @@
+﻿# Import the Azure PowerShell module.
+Import-Module -Name Az
+
+# Connect to an Azure account.
+Connect-AzAccount
+
+# Define Azure variables for a virtual machine
+$vmName = "WingtiptoyysVM"
+$resourceGroup = "WingtiptoyysVM"
+
+# Create Azure credentials.
+$adminCredential = Get-Credential -Message "Enter a username and password for the VM administrator."
+
+# Creat a vistual machine in Azure
+New-AzVM -ResourceGroupName $resourceGroup -Name $vmName -Credential $adminCredential -Image UbuntuLTS
+
